34512d798fc404f4953d35a296fd3ee274d15502
fuzhenghua <fuzhenghua@users.noreply.github.com>
2014-09-12 18:15:37 -0400

    Update qtest_shm.cpp

--- src/util/test/qtest_shm.cpp
+++ src/util/test/qtest_shm.cpp
@@ -1,43 +1,68 @@
 #include "qtest.h"
 
-typedef SwQueue<QLEN, DATALEN, ShmCircularBuffer> QType;
+#define QItems (1024*64)
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("Usage: %s read[0/1], num_readers|writer_wait(micro), init_to_zero[0/1]\n", argv[0]);
-        return 0;
-    }
+typedef SwQueue<QLEN, DATALEN, ShmCircularBuffer> SQType;
+typedef MwQueue2<DATALEN, QItems, ShmCircularBuffer> MQType;
 
-    bool is_read = (atoi(argv[1]) == 1);
-    int param1 = atoi(argv[2]);
-    bool init_to_zero = (argc>3)?(atoi(argv[3]) == 1) : false;
-
-    int num_readers = param1;
-    int wait_micro = param1;
-
-    int itemCount = ITEMCNT;
-
-    QType queue("qtest", is_read, init_to_zero);
-
+template<typename QType>
+void runTest(QType& queue, bool is_read, int num_threads, int itemCount, int wait_micro, int writer_start_id) {
     if (is_read) {
-        for (int i=0; i<num_readers; ++i)
+        for (int i=0; i<num_threads; ++i)
         {
-            QueueReader< QType::Reader >* reader =
-                    new QueueReader< QType::Reader >(i, *queue.newReader());
-            ThreadWrapper< QueueReader< QType::Reader > >* readerThread =
-                    new ThreadWrapper< QueueReader< QType::Reader > >(*reader);
+            QueueReader< typename QType::Reader >* reader =
+                    new QueueReader< typename QType::Reader >(i, *queue.newReader());
+            ThreadWrapper< QueueReader< typename QType::Reader > >* readerThread =
+                    new ThreadWrapper< QueueReader< typename QType::Reader > >(*reader);
 
             readerThread->run(&itemCount);
         }
         while (1) {
             sleep (1000000);
         }
+        return;
     }
 
+    for (int i=0; i<num_threads; ++i)
+    {
+        QueueWriter< typename QType::Writer > *writer =
+                new QueueWriter< typename QType::Writer >(writer_start_id+i, queue.theWriter(), wait_micro);
+        ThreadWrapper< QueueWriter< typename QType::Writer > >* writerThread =
+                new ThreadWrapper< QueueWriter< typename QType::Writer > >(*writer);
+        writerThread->run(&itemCount);
+    }
+    while (1) {
+        sleep (1000000);
+    }
 
-    QueueWriter< QType::Writer > writer (0, queue.theWriter(), wait_micro);
-    ThreadWrapper< QueueWriter< QType::Writer > > writerThread(writer);
-    writerThread.run(&itemCount);
-    writerThread.join();
+}
+
+int main(int argc, char** argv) {
+    if (argc < 7) {
+        printf("Usage: %s read[0/1], num_threads writer_wait(micro), init_to_zero[0/1], qtype[s/m] writer_start_id\n", argv[0]);
+        return 0;
+    }
+
+    bool is_read = (atoi(argv[1]) == 1);
+    int num_readers = atoi(argv[2]);
+    int wait_micro = atoi(argv[3]);
+    bool init_to_zero = (atoi(argv[4]) == 1);
+    char qtchar = argv[5][0];
+    int writer_start_id = atoi(argv[6]);
+
+    int itemCount = ITEMCNT;
+
+    switch(qtchar) {
+    case 's' : {
+        SQType queue("qtest", is_read, init_to_zero);
+        runTest(queue, is_read, num_readers, itemCount, wait_micro, writer_start_id);
+        break;
+    }
+    case 'm' : {
+        MQType queue("qtest", is_read, init_to_zero);
+        runTest(queue, is_read, num_readers, itemCount, wait_micro, writer_start_id);
+        break;
+    }
+    }
     return 0;
 }
